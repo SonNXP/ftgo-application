25db67f52dcb7356f0a080fa2d8b6c8d8848c5a9
SonNXP <son.nguyen.xp12@gmail.com>
2021-05-11 11:43:15 +0700

    Unit test, integration test for REST API, Pub/Sub, async request/response

--- ftgo-order-service/src/integration-test/java/net/chrisrichardson/ftgo/orderservice/domain/OrderJpaTest.java
+++ ftgo-order-service/src/integration-test/java/net/chrisrichardson/ftgo/orderservice/domain/OrderJpaTest.java
@@ -22,18 +22,30 @@ public class OrderJpaTest {
   @Autowired
   private OrderRepository orderRepository;
 
+  // TransactionTemplate provides a set of callback-based APIs to manage
+  // transactions manually.
   @Autowired
   private TransactionTemplate transactionTemplate;
 
+  // The shouldSaveAndLoadOrder() test method executes two transactions. The first
+  // saves a newly created Order in the database. The second transaction loads the
+  // Order and verifies that its fields are properly initialized.
   @Test
   public void shouldSaveAndLoadOrder() {
 
+    // Execute the action specified by the given callback object within a
+    // transaction. Allows for returning a result object created within the
+    // transaction, that is, a domain object or a collection of domain objects. A
+    // RuntimeException thrown by the callback is treated as a fatal exception that
+    // enforces a rollback. Such an exception gets propagated to the caller of the
+    // template.
     long orderId = transactionTemplate.execute((ts) -> {
-      Order order = new Order(CONSUMER_ID, AJANTA_ID, OrderDetailsMother.DELIVERY_INFORMATION, chickenVindalooLineItems());
+      Order order = new Order(CONSUMER_ID, AJANTA_ID, OrderDetailsMother.DELIVERY_INFORMATION,
+          chickenVindalooLineItems());
+      // run an instance of the database during testing is to use Docker.
       orderRepository.save(order);
       return order.getId();
     });
-
 
     transactionTemplate.execute((ts) -> {
       Order order = orderRepository.findById(orderId).get();
